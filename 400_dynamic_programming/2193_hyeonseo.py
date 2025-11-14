f876b05a76a6350a69a2e9aa51fd4a8a2c4674fa
tthnso <tthnso@gmail.com>
2025-11-14 16:51:31 +0900

    remove

--- 400_dynamic_programming/2193_hyeonseo.py
+++
@@ -1,7 +0,0 @@
-pinary = [1, 1] + [0 for _ in range(90)]
-
-for i in range(2, 90):
-    pinary[i] = pinary[i-1] + pinary[i-2]
-
-number = int(input())
-print(pinary[number - 1])
--- 400_dynamic_programming/2225_hyeonseo.py
+++
@@ -1,9 +0,0 @@
-# 시간 초과
-def decompose_sum(n, k):
-    if k == 1:
-        return 1
-    else:
-        return sum(decompose_sum(n - i, k - 1) for i in range(n + 1))
-
-N, K = map(int, input().split())
-print(decompose_sum(N, K))
